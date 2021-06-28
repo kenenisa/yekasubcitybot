d5618ec4bb56e35f649eaa95101374468920620d
eba <ebaalemayhu3@gmail.com>
2021-06-28 17:28:37 +0300

    fixed hungup issue

--- index.js
+++ index.js
@@ -16,12 +16,20 @@ app.get('/', function (req, res) {
 // 
 app.post('/1765703667:AAE1rxOjZfXEdba1O29k_IuNcTK41vkqDsc', function (req, res) {
   const message = req.body.message;
-  // console.log(message)
+  console.log('----' + message.text,message.chat.first_name);
   post('sendMessage',
     {
       chat_id: message.chat.id,
-      text: 'Bot is Live :) your message: ' + message.text + '--your name is: ' + message.chat.first_name + '-- chat id:' + message.chat.id
-    })
+      text: 'Bot is Live :) your message: ' + message.text + '--your name is: ' + message.chat.first_name + '-- chat id:' + message.chat.id,
+      reply_markup:{
+        keyboard:[
+          {text:'Say yes'},
+          {text:'Say no'}
+        ]
+      }
+    });
+
+  res.end("yes");
 })
 app.set("port", process.env.PORT || 3000);
 app.set("host", process.env.HOST || "localhost");
