5a7289566664fb31987ba24c65342bfa905ed575
eba <ebaalemayhu3@gmail.com>
2021-06-28 16:01:21 +0300

    update

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ const app = express();
 app.get('/',function(req,res){
     res.send('<h1>hello world</h1>');
 })
-
-app.listen(5000);
+const port = 80;
+app.listen(port,function(){
+    console.log('app running at: ' + port);
+});
 // [In reply to Keni]
 // Elias habib
 // Feeliasconst@gmail.com
